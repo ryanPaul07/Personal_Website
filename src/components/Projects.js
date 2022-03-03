e2c30261db804ad0d9efecd648ae42e3a433224d
Ryan Paul <rpaul06@uoguelph.ca>
2022-03-02 23:25:00 -0500

    added new design project and fixed alignment

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Card, CardContent } from '@material-ui/core';
 import projectMovie from '../resources/Movie-App.gif';
 import projectDiscoverTracks from '../resources/Discover-Tracks-App.gif';
+import projectDesignComparison from '../resources/designComp.gif';
 
 var projects = [{
     link: "https://github.com/ryanPaul07/MovieManager",
@@ -14,6 +15,12 @@ var projects = [{
     languages: "REACT * Spotify API * Tumblr API * OAuth 2.0",
     imgSRC: projectDiscoverTracks,
     alt: "Discover Tracks Project"
+  },
+  {
+    link: "https://www.behance.net/gallery/138564749/Flat-vs-Claymorphism-vs-Neumorphism?",
+    languages: "Design * UI/UX * HCI",
+    imgSRC: projectDesignComparison,
+    alt: "Flat vs Claymorphism vs Neumorphism"
   }
 ]
 
@@ -30,10 +37,10 @@ function Projects() {
         <div style={{margin: "20px 0 20px 0"}}>
           <p style={{color: '#cd3434', fontSize:"22px"}}>
           Here are some of the projects I’ve worked on, however all my other projects are available on <a className="project-github-link" href="https://github.com/ryanPaul07" target="_blank" rel="noopener noreferrer" style={{color: '#c11717', fontFamily: 'IBM Plex Serif', fontWeight: "bold" ,textDecoration: 'none'}}>Github</a> 
-           .Design website coming soom :o
+           . Design website coming soom :o
           </p>
         </div>
-        <div className="card-view">
+        <div className="card-view" style={{justifyContent: "center"}}>
           {
             projects.map((project, i)=> {
               return <Card key={i} className="card-styling" onClick={()=> showProject(project)}>
