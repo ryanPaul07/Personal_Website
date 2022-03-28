de502ad493493ea888894b9df1f566823a8897ae
Ryan Paul <rpaul06@uoguelph.ca>
2022-03-28 12:33:38 -0400

    Updates

--- precache-manifest.699571f3148a6fd7b1436b85f5ccddbe.js
+++ precache-manifest.699571f3148a6fd7b1436b85f5ccddbe.js
@@ -1,31 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e0db50a735a5c2a5cc1aff0097349578",
+    "revision": "65deb81869a73e594b206ede5dff2cf4",
     "url": "/index.html"
   },
   {
-    "revision": "c034bd5b6406455f4dbf",
-    "url": "/static/css/main.2fdc3149.chunk.css"
+    "revision": "e0f6d9a47f8aa3853886",
+    "url": "/static/css/main.d8c29aa0.chunk.css"
   },
   {
-    "revision": "7e558a040a331e98a49a",
-    "url": "/static/js/2.47c03bfc.chunk.js"
+    "revision": "7a1623d5d9e7c80594c9",
+    "url": "/static/js/2.b927cd61.chunk.js"
   },
   {
     "revision": "6ef57ea73f859e79d7f696b27898a6e5",
-    "url": "/static/js/2.47c03bfc.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.b927cd61.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c034bd5b6406455f4dbf",
-    "url": "/static/js/main.c729cdbb.chunk.js"
+    "revision": "e0f6d9a47f8aa3853886",
+    "url": "/static/js/main.4f9ec6e6.chunk.js"
   },
   {
     "revision": "506adfa26ad76816c4a2",
     "url": "/static/js/runtime-main.c34c0ec6.js"
-  },
-  {
-    "revision": "4c1fa7547576e72ce2c85b3a2b4260ea",
-    "url": "/static/media/Apathy.4c1fa754.png"
   },
   {
     "revision": "dd95cef9b862e02a30915ee24f95d27b",
@@ -36,16 +32,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/Movie-App.c255f653.gif"
   },
   {
-    "revision": "0b980d3a78cc1516b5a9b5da2830e692",
-    "url": "/static/media/SocisDesign(Mac).0b980d3a.png"
-  },
-  {
     "revision": "4134f082d96f879bfb79f948a1eda14a",
     "url": "/static/media/ZoomChat.4134f082.png"
-  },
-  {
-    "revision": "ec4989c0a741890fa54034f412d14094",
-    "url": "/static/media/bean.ec4989c0.png"
   },
   {
     "revision": "1521b6f1e049133d08d86ab54c72b1bd",
@@ -54,10 +42,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "a17392f762406b8a2bd55c6cde544af8",
     "url": "/static/media/designComp.a17392f7.gif"
-  },
-  {
-    "revision": "a0cbc5ad44d5fe4c00b7b9c017035a13",
-    "url": "/static/media/ryan.a0cbc5ad.png"
   },
   {
     "revision": "042dea3f980aad6caeb360613c2fd502",
